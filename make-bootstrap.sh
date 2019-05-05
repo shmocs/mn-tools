add276953ebf5bea9bcd9f73a6ebb18382ba86b9
Shmocs <costache.sergiu@gmail.com>
2019-05-05 23:52:38 +0300

    removed local zip after boostraping & rsyncing

--- make-bootstrap.sh
+++ make-bootstrap.sh
@@ -9,3 +9,4 @@ zip -r snowgem-indexed-chain-$block.zip blocks chainstate
 ssh $webhost "cd $webroot && rm -f *.zip"
 scp snowgem-indexed-chain-$block.zip $webhost:$webroot
 ssh $webhost "cd $webroot && ln -s snowgem-indexed-chain-$block.zip indexed-chain.zip"
+rm -f snowgem-indexed-chain-$block.zip
